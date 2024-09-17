f001c9273346264ef521ae22525c33070adca37a
Alex <alexandr.i.semenov@gmail.com>
2024-09-17 16:05:37 +0400

    added settings

--- assets/Scripts/GameParameters.js
+++ assets/Scripts/GameParameters.js
@@ -1,12 +1,19 @@
 export default class {
-	static columns = 12; // N
-	static rows = 12; // M
+	static columns = 6; // N
+	static rows = 6; // M
 	static colorOptionsAmount = 5; // C
 	static minTilesToBlast = 2; // K
 	static shuffleCount = 1; // S
 	static startMovesCount = 40; // Y
-	static targetPoints = 300000; // X
+	static targetPoints = 30000; // X
 	static bombRadius = 2; // R
+
+	static minColumns = 2;
+	static maxColumns = 12;
+	static minRows = 2;
+	static maxRows = 12;
+	static minColorOptionsAmount = 1;
+	static maxColorOptionsAmount = 5;
 
 	static tileSize = { width: 171, height: 192 };
 	static tileMoveSpeed = 2500;
